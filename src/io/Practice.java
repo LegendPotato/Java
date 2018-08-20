5f506cb42e386b42d85347776c2e25e943f74b58
zhangshuai_sx <zhangshuai_sx@qiyi.com>
2018-08-20 21:49:24 +0800

    20180820

--- src/io/Practice.java
+++ src/io/Practice.java
@@ -3,9 +3,7 @@ package io;
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Formatter;
-import java.util.Map;
+import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
@@ -45,7 +43,16 @@ public class Practice {
 //            }
 //        }
         System.out.println(new Formatter().format("%1$4d - the year of %2$f",956,Math.PI));
+        List list = new ArrayList();
 
+        while (true){
+            list.add(new T());
+        }
 
+    }
+    public static class T{
+        public T(){
+
+        }
     }
 }
---
+++ src/test/Main.java
@@ -0,0 +1,57 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+
+    //封装点坐标
+    static class Point {
+        int x, y;
+        boolean isVisited;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.isVisited = false;
+        }
+
+        public int getLength(Point point) {
+            return Math.abs(this.x - point.x) + Math.abs(this.y - point.y);
+        }
+    }
+
+    static int minpath = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int count = Integer.parseInt(sc.nextLine().trim());
+
+        Point[] points = new Point[count];
+        for (int i = 0; i < count; i++) {
+            String[] locations = sc.nextLine().trim().split(",");
+            points[i] = new Point(Integer.parseInt(locations[0]), Integer.parseInt(locations[1]));
+        }
+        Point start = new Point(0, 0);
+        Main.dfs(start, points, 0, 0);
+        System.out.println(minpath);
+    }
+
+    public static void dfs(Point start, Point[] points, int sum, int count) {
+        if (count == points.length) {
+            sum += start.getLength(new Point(0, 0));
+            minpath = Math.min(minpath, sum);
+            return;
+        }
+
+        for (int i = 0; i < points.length; i++) {
+            if (!points[i].isVisited) {
+                points[i].isVisited = true;
+                dfs(points[i], points, sum + start.getLength(points[i]), count + 1);
+                points[i].isVisited = false;
+            }
+        }
+    }
+
+
+}
