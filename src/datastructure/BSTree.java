6c19145511756795280ca50218468914b629aff3
LegendPotato <zhangs1222@qq.com>
2018-08-23 23:13:54 +0800

    0823-03

--- src/datastructure/BSTree.java
+++ src/datastructure/BSTree.java
@@ -1,12 +1,16 @@
 package datastructure;
 
-public class BSTree{
+public class BSTree {
 
     //根节点
-    private BSTNode node;
+    private BSTNode root;
+
+    public BSTree() {
+
+    }
 
     //节点定义，通过一个内部类来定义
-    public class BSTNode{
+    public class BSTNode {
 
         //key值和左右孩子节点，和父节点
         int key;
@@ -14,18 +18,76 @@ public class BSTree{
         BSTNode right;
         BSTNode parent;
 
-        public BSTNode(int key, BSTNode left, BSTNode right, BSTNode parent){
+        public BSTNode(int key, BSTNode left, BSTNode right, BSTNode parent) {
             this.key = key;
             this.left = left;
             this.right = right;
             this.parent = parent;
         }
-
     }
 
     //前序遍历
-    private void preOrder(BSTNode tree){
-
+    public void preOrder(BSTNode root) {
+        if (root != null) {
+            System.out.println(root.key);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
+
+    //中序遍历
+    public void midOrder(BSTNode root) {
+        if (root != null) {
+            midOrder(root.left);
+            System.out.println(root.key);
+            midOrder(root.right);
+        }
+    }
+
+    //后序遍历
+    public void postOrder(BSTNode root) {
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.println(root.key);
+        }
+    }
+
+    //初始化一个以root为根的二叉搜索树
+    public BSTree(BSTNode root) {
+        this.root = root;
+    }
+
+    //插入一个元素到树中
+    public void addLeaf(BSTNode root, BSTNode leaf) {
+        if (leaf.key < root.key) {
+            if (root.left == null) {
+                root.left = leaf;
+            } else {
+                addLeaf(root.left, leaf);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = leaf;
+            } else {
+                addLeaf(root.right, leaf);
+            }
+        }
+    }
+
+    //查找key值
+    public BSTNode search(BSTNode root, int key) {
+        if (root == null) return null;
+        else {
+            if (key == root.key) {
+                return root;
+            } else if (key < root.key) {
+                return search(root.left, key);
+            } else {
+                return search(root.right, key);
+            }
+        }
+    }
+
 
 }
---
+++ src/datastructure/BSTreeTest.java
@@ -0,0 +1,8 @@
+package datastructure;
+
+public class BSTreeTest {
+    public static void main(String[] args) {
+        BSTree tree = new BSTree();
+
+    }
+}
